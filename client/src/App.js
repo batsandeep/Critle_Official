18f5140b11bac97f35afe8260c28565968c62bf2
Batchala Sandeep <batsandeep@gmail.com>
2020-06-13 12:24:22 +0530

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React,{useEffect} from 'react';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/HomePage/Home';
+import Profile from './components/Profile';
+import Upload from './components/UploadPage/Upload';
+import {BrowserRouter,Route} from 'react-router-dom';
+import {connect} from 'react-redux'
+import {fetchUserAction} from './actions/myaction'
+function App(props) {
+    useEffect(()=>{
+        props.fetch_user()
+    },[])
+  return (
+    <Upload />
+
+    // <BrowserRouter>
+    //   <Header />
+    //   <Route exact path="/" component={Home} />
+    //   <Route path="/profile" component={Profile} />
+    //   <Route path="/upload" component={Upload} />
+    // </BrowserRouter>
+  );
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return{
+        fetch_user:()=>{dispatch(fetchUserAction())}
+    }
+}
+
+export default connect(null,mapDispatchToProps)(App);
